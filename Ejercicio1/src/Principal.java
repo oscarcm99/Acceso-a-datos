31c0a96357615d5b9ef25b0d84c02dd5d3d444dc
oscarcm99 <gorri0302@gmail.com>
2018-10-08 13:24:32 +0200

    Clase

--- Ejercicio1/src/Principal.java
+++ Ejercicio1/src/Principal.java
@@ -8,9 +8,12 @@ public class Principal {
 		int den2 = 0;
 		int num1 = 0;
 		Operacion op =  new Operacion(num1, num2, den1, den2);
-		
+		long numdni= 0;
+		Nif nif =  new Nif(numdni);
 		double resultado = op.sumar(1, 1, 1, 1);
 		System.out.println(resultado);
+		char letra  = nif.calcularLetra(50489472);
+		System.out.println(letra);
 	}
 
 }
